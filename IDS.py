efd32ae5f865e50bb675b55c45cb849ceaf3e9a5
KostasEreksonas <k.ereksonas@gmail.com>
2023-04-10 12:49:17 +0300

    Finding categorical features

--- IDS.py
+++ IDS.py
@@ -10,26 +10,32 @@ import matplotlib as plt
 import seaborn as sns
 import time
 
-def get_columns(filename):
-    """Get features list to columns array"""
-    cols = []
-    # Read features list
-    with open(filename, 'r') as file:
-        next(file) # Start from the second row
-        for row in file:
-            cols.append(row.split()[0].rstrip(':'))
-        cols.append("target")
-    print(cols)
+cols = []
+# Read features list
+with open("data/kddcup.names", 'r') as file:
+    next(file) # Start from the second row
+    for row in file:
+        cols.append(row.split()[0].rstrip(':'))
+    cols.append("target")
 
-def get_attacks(filename):
-    """Create a dictionary of attack types"""
-    attack_types = {}
-    with open(filename, 'r') as file:
-        attack_types['normal'] = 'normal'
-        for row in file:
-            (key, val) = row.rstrip("\n").split(' ')
-            attack_types[key] = val
-    print(attack_types)
+attack_types = {}
+with open("data/training_attack_types", 'r') as file:
+    attack_types['normal'] = 'normal'
+    for row in file:
+        (key, val) = row.rstrip("\n").split(' ')
+        attack_types[key] = val
 
-get_columns("data/kddcup.names")
-get_attacks("data/training_attack_types")
+df = pd.read_csv("data/kddcup.data_10_percent.gz", names = cols)
+# Add attack type column
+df['Attack Type'] = df.target.apply(lambda r:attack_types[r[:-1]])
+#print(df.shape)
+#print(df.isnull().sum())
+
+# Find categorical features
+num_cols = df._get_numeric_data().columns
+
+cate_cols = list(set(df.columns)-set(num_cols))
+cate_cols.remove('target')
+cate_cols.remove('Attack Type')
+
+print(cate_cols)
